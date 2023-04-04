76b393de4b47fcc400339c0ac1d73fe8d74000da
Doga Kihtir <dkihtir@hotmail.com>
2023-04-04 20:51:03 +0300

    First example of structures Figure 10.3 added.

---
+++ Structures/fig10_03.cpp
@@ -0,0 +1,77 @@
+// Fig. 10.3: fig10_03.c
+// Card shuffling and dealing program using structures
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define CARDS 52
+#define FACES 13
+
+// card structure definition
+struct card {
+    const char *face;
+    const char *suit;
+};
+
+// new type name for struct card
+typedef struct card Card;
+
+// prototypes
+void fillDeck(Card *const wDeck, const char *wFace[], const char *wSuit[]);
+void shuffle(Card *const wDeck);
+void deal(const Card *const wDeck);
+
+int main(void)
+{
+    // define array of Cards
+    Card deck[CARDS];
+
+    // initialize array of pointers
+    const char *face[] = { "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", 
+    "Eight", "Nine", "Ten","Jack", "Queen", "King"};
+ 
+    // initialize array of pointers
+    const char *suit[] = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+    srand(time(NULL));
+
+    fillDeck(deck,face,suit); // load the deck with Cards
+    shuffle(deck); // put Cards in random order
+    deal(deck); // deal all 52 Cards
+
+    return 0;
+}
+
+// place strings into Card structures
+void fillDeck(Card *const wDeck, const char *wFace[], const char *wSuit[])
+{
+    // loop through wDeck
+    for (size_t i = 0; i<CARDS; ++i) 
+    {
+        wDeck[i].face = wFace[i%FACES];
+        wDeck[i].suit = wSuit[i/FACES];
+    }
+}
+
+// shuffle cards
+void shuffle(Card *const wDeck)
+{
+    // loop through wDeck randomly swapping Cards
+    for (size_t i = 0; i<CARDS; ++i)
+    {
+        size_t j = rand()%CARDS;
+        Card temp = wDeck[i];
+        wDeck[i] = wDeck[j];
+        wDeck[j] = temp;
+    }
+}
+
+// deal cards
+void deal(const Card *const wDeck)
+{
+    for (size_t i = 0; i<CARDS;++i)
+    {
+        printf("%5s of %-8s%s",wDeck[i].face,wDeck[i].suit,(i+1) % 4 ? " ":"\n");
+    }
+}
